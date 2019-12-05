b31d4d814ee52658c1a736f5566349a95f27f08e
Viky <vikymuhamad@gmail.com>
2019-12-06 01:46:41 +0700

    Menyambungkan API ke front end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
+import NavbarComponent from "./components/Navbar.component";
+import EditExercisesComponent from "./components/EditExercises.component";
+import ExercisesListComponent from "./components/ExercisesList.component";
+import CreateExercises from "./components/CreateExercises.component";
+import CreateUsersComponent from "./components/CreateUsers.component";
 
 function App() {
   return (
-    <div className="App">
-        <h1>kowkwwk</h1>
-    </div>
+   <Router>
+       <div className="container">
+       <NavbarComponent />
+       <br/>
+       <Route path="/" exact component={ExercisesListComponent} />
+       <Route path="/edit/:id"  component={EditExercisesComponent} />
+       <Route path="/create"  component={CreateExercises} />
+       <Route path="/user"  component={CreateUsersComponent} />
+       </div>
+   </Router>
   );
 }
 
